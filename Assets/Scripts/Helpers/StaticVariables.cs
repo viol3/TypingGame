548fa5f17d9e2eb80f16a3b8c4a656f8db798c69
viol33 <alininavcisi@gmail.com>
2020-10-18 13:17:39 +0300

    simplified to use as module

--- Assets/Scripts/Helpers/StaticVariables.cs
+++
@@ -1,6 +0,0 @@
-﻿
-public static class StaticVariables
-{
-    public static float ROTATESPEED = 300f;
-    public static float ROCKETSPEED = 8f;
-}
--- Assets/SimpleTypingGameManager.cs
+++ Assets/SimpleTypingGameManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class SimpleTypingGameManager : MonoBehaviour
 {
     [SerializeField] private TouchScreenKeyboardCharacterInput _touchScreenKeyboardCharacterInput;
     [SerializeField] private TextAsset _wordListFile;
